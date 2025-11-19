631bf71172818c18d983d347ac436a779ed3c6fd
Chanu <kollichanu@gmail.com>
2025-11-18 20:52:20 -0600

    Added comments into redirections-command.sh

--- shell-scripting/redirections-command.sh
+++ shell-scripting/redirections-command.sh
@@ -1,3 +1,4 @@
 echo "Hello World!" > /tmp/out.txt
 
+#this will print first command output into out1.txt (ls -ltr /boot) and error into error file
 ls -ltr /boot /boot1 >/tmp/out1.txt 2>/tmp/error.txt
